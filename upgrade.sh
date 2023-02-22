a3ebdd32e296b3a9bab2afcc2b1d98849d79cddb
deba.jena <105785282+Debaprasadjena1997@users.noreply.github.com>
2023-02-22 11:50:35 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -35,15 +35,6 @@ echo "check .problem directory"
 if [ -d "/home/pi/piSignagePro.problem" ]; then
 	sudo rm -rf  /home/pi/piSignagePro.problem
 fi
-echo "deleting updatevrquin.prev directory if exists"
-if [ -d "/home/pi/updatevrquin.prev" ]; then
-    sudo rm -rf  /home/pi/updatevrquin.prev
-fi
-echo "saving the current updatevrquin"
-mv /home/pi/updatevrquin /home/pi/updatevrquin.prev
-
-git clone https://github.com/Debaprasadjena1997/updatevrquin.git
-# git clone https://github.com/GramThanos/captive-portal.git
 
 echo "get the new server release file"
 ping -c 3 www.pisignage.com
@@ -82,7 +73,7 @@ echo "saving the current updatevrquin"
 mv /home/pi/updatevrquin /home/pi/updatevrquin.prev
 
 git clone https://github.com/Debaprasadjena1997/updatevrquin.git
-git clone https://github.com/Debaprasadjena1997/captive-portal.git
+# git clone https://github.com/GramThanos/captive-portal.git
 
 echo "unzipping the New pi image"
 rm -rf  /home/pi/piImage
